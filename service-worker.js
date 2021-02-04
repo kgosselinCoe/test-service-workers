588558b41dbc3e6bc2c0347134b8fcbd54654133
Kévin <kgosselin@coefficy.com>
2021-02-04 15:13:42 +0100

    worker install

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,14 @@
 (() =>{
 	if('serviceWorker' in navigator){
-		navigator.serviceWorker.register('/sw-test/sw.js', {scope: '/'})
+		navigator.serviceWorker.register('/sw-test/sw.js', {scope: '/sw-test/'})
 		.then((reg) => {
+			if(reg.installing) {
+				console.log('Service worker installing');
+			} else if(reg.waiting) {
+				console.log('Service worker installed');
+			} else if(reg.active) {
+				console.log('Service worker active');
+			}
 			console.log('Registration succeeded. Scope is ' + reg.scope);
 		}).catch(function(error) {
 			// registration failed
