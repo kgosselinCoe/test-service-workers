8c2e73699a4b09f75931bae901312f670b939ea1
Kévin <kgosselin@coefficy.com>
2021-02-04 15:36:01 +0100

    worker install

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -14,6 +14,9 @@ self.addEventListener('install', function (event) {
        })
    );
 });
+self.addEventListener("activate", function(event) {
+    event.waitUntil(self.clients.claim());
+});
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
